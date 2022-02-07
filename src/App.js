5e482e6b6647ab63221a38ec5c6633929266c569
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-06 21:52:34 -0400

    create Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/nav-bar/NavBar';
+import { Routes, Route } from "react-router-dom";
+import Home from './components/home/Home';
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <Routes>
+        <Route exact path="/" element={ <Home /> } />
+      </Routes>
     </>
   );
 };
