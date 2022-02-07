c3ad3053673f33859db2c3eafaee33cfd563d168
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-06 22:08:13 -0400

    create About component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.css";
 import FoodImages from "../food-images/FoodImages";
+import About from "../about/About";
 
 const Home = () => {
     return (
         <div>
             <FoodImages />
+            <About />
         </div>
     );
 };
