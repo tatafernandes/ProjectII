65f32d1d760f0698d5a472ea8529f39a0b907397
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-08 21:02:34 -0400

    temporaty idFoodsList

--- src/components/foods-list/FoodsList.js
+++ src/components/foods-list/FoodsList.js
@@ -5,11 +5,11 @@ import FoodCard from "../food-card/FoodCard";
 
 const FoodsList = () => {
     const { pathname } = useLocation();
-    const id = pathname.split("/").slice(2)[0];
+    const idFoodsList = pathname.split("/").slice(2)[0];
 
     return (
         <div>
-            <h1>FoodsList.js - {id}</h1>
+            <h1>FoodsList.js - {idFoodsList}</h1>
             <FoodCard />
         </div>
     );
