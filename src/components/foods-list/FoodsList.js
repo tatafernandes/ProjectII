ad5f0c40a9f669d74371d6721cec26edf6f4b312
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-08 21:22:22 -0400

    getMainCourse function

--- src/components/foods-list/FoodsList.js
+++ src/components/foods-list/FoodsList.js
@@ -16,6 +16,37 @@ class FoodsApi {
             throw new Error("Cannot Fetch Dessert");
         };
     };
+    getMainCourse = async () => {
+        try {            
+            const { data: { meals: beef } } = await this.api.get("/filter.php?c=Beef");
+            const { data: { meals: chicken } } = await this.api.get("/filter.php?c=Chicken");
+            const { data: { meals: lamb } } = await this.api.get("/filter.php?c=Lamb");
+            const { data: { meals: miscellaneous } } = await this.api.get("/filter.php?c=Miscellaneous");
+            const { data: { meals: pasta } } = await this.api.get("/filter.php?c=Pasta");
+            const { data: { meals: pork } } = await this.api.get("/filter.php?c=Pork");
+            const { data: { meals: seafood } } = await this.api.get("/filter.php?c=Seafood");
+            const { data: { meals: vegan } } = await this.api.get("/filter.php?c=Vegan");
+            const { data: { meals: vegetarian } } = await this.api.get("/filter.php?c=Vegetarian");
+            const { data: { meals: goat } } = await this.api.get("/filter.php?c=Goat");
+
+            const mainCourse = [
+                ...beef,
+                ...chicken,
+                ...lamb,
+                ...miscellaneous,
+                ...pasta,
+                ...pork,
+                ...seafood,
+                ...vegan,
+                ...vegetarian,
+                ...goat
+            ];
+
+            return mainCourse;
+        } catch {
+            throw new Error("Cannot Fetch MainCourse");
+        };
+    };
 };
 
 const foodsApi = new FoodsApi();
@@ -25,7 +56,7 @@ const FoodsList = () => {
     const idFoodsList = pathname.split("/").slice(2)[0];
     const [foodCards, setFoodCards] = useState([]);
     
-    useEffect(() => (async () => setFoodCards(await foodsApi.getDessert()))(), [idFoodsList]);
+    useEffect(() => (async () => setFoodCards(await foodsApi.getMainCourse()))(), [idFoodsList]);
 
     return (
         <div>
