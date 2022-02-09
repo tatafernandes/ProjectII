4e064bbc848778784c9ac5d0d16172ab60d479d5
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-08 21:01:00 -0400

    temporary id

--- src/components/foods-list/FoodsList.js
+++ src/components/foods-list/FoodsList.js
@@ -5,10 +5,11 @@ import FoodCard from "../food-card/FoodCard";
 
 const FoodsList = () => {
     const { pathname } = useLocation();
+    const id = pathname.split("/").slice(2)[0];
 
     return (
         <div>
-            <h1>FoodsList.js - {pathname}</h1>
+            <h1>FoodsList.js - {id}</h1>
             <FoodCard />
         </div>
     );
