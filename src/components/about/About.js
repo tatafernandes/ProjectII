85311886111b25931cc98b306d3082f93213b9f7
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-10 20:55:23 -0400

    adjust branch

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,19 +3,7 @@ import "./About.css";
 
 const About = () => {
     return (
-        <div className='about-us'>
-        <div>
         <h2>About.js</h2>
-
-        <h2>Thais Fernandes</h2>
-        <p>Brazilian, living in Portugal and trying to be a Web Development and a great mom! </p>
-       </div>
-
-       <div>
-       <h2>Vinicius Leite Batista</h2>
-       <p>Brazilia, from Boa Vista - Roraima, civil engineer student and Ironhacker Web Dev to be!</p>
-       </div>
-       </div>
     );
 };
 
--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
- export const NavBar = () => {
-    return <nav className=navbar-style>=
-        
-        <NavLink to='/'><span> LOGO </span></NavLink>
-        <NavLink to='/foods-list/main-course'> Main Course </NavLink>
-        <NavLink to='/foods-list/dessert'> Dessert </NavLink>
-</nav> 
+const NavBar = () => {
+    return (
+        <h1>NavBar.js</h1>
+    );
 };
 
 export default NavBar;
