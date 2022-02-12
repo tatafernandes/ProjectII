f27a9576ae1aaf76b7548bbd37b596be9d0cea7a
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-12 14:53:10 -0400

    create getIngredients func

--- src/components/ingredients/Ingredients.js
+++ src/components/ingredients/Ingredients.js
@@ -6,11 +6,19 @@ class FoodsApi {
     constructor () {
         this.api = axios.create({ baseURL: "https://www.themealdb.com/api/json/v1/1" });
     };
-    getRecipe = async id => {
+    getIngredients = async id => {
         try {
             const { data: { meals } } = await this.api.get(`/lookup.php?i=${id}`);
 
-            return meals[0];
+            const ingredients1 = [];
+            for (let i = 1; i <= 20; i += 1) {
+                const measure = await meals[0][`strMeasure${i}`];
+                const ingredient = await meals[0][`strIngredient${i}`];
+
+                ingredients1.push(`${measure} ${ingredient}`)
+            };
+
+            return ingredients1;
         } catch (error) {
             throw new Error(`Cannot Fetch Recipe => ${error}`);
         };
@@ -20,15 +28,16 @@ class FoodsApi {
 const foodsApi = new FoodsApi();
 
 const Ingredients = ({ idRecipe }) => {
-    const [ingredients, setIngredients] = useState({});
+    const [ingredients, setIngredients] = useState([]);
 
-    useEffect(() => (async () => setIngredients(await foodsApi.getRecipe(idRecipe)))(), [idRecipe])
+    useEffect(() => (async () => setIngredients(await foodsApi.getIngredients(idRecipe)))(), [idRecipe]);
+    console.log(ingredients);
 
     return (
         <div>
             <h2>Ingredients</h2>
             <ul>
-                <li>{ingredients.strMeasure1} {ingredients.strIngredient1}</li>
+                {ingredients.map((ingredient, i) => <li key={i}>{ingredient}</li>)}
             </ul>
         </div>
     );
