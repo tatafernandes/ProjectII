7203d5b2fa98df27ab160ddba1a4dfc139a43f8f
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-12 14:22:17 -0400

    test api

--- src/components/food-recipe/FoodRecipe.js
+++ src/components/food-recipe/FoodRecipe.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./FoodRecipe.css";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+
+class FoodsApi {
+    constructor () {
+        this.api = axios.create({ baseURL: "https://www.themealdb.com/api/json/v1/1" });
+    };
+    getRecipe = async id => {
+        try {
+            const { data: { meals } } = await this.api.get(`/lookup.php?i=${id}`);
+
+            return meals[0];
+        } catch (error) {
+            throw new Error(`Cannot Fetch Recipe => ${error}`);
+        };
+    };
+};
+
+const foodApi = new FoodsApi();
 
 const FoodRecipe = () => {
     const { idRecipe } = useParams();
+    const [recipe, setRecipe] = useState([]);
+
+    useEffect(() => (async () => setRecipe(await foodApi.getRecipe(idRecipe)))(), [idRecipe]);
+    console.log(recipe);
 
     return (
-        <h1>FoodRecipe.js {idRecipe}</h1>
+        <>
+            {recipe.strMeal}
+        </>
     );
 };
 
