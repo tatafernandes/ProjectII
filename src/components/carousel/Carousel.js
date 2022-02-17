002c4b52563de5f08ddf53468e795a97e404bcd5
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-17 18:58:21 -0400

    create Categories and Carousel components

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Carousel = ( {strCategory} ) => {
+    return (
+        <h3>{strCategory}</h3>
+    );
+};
+
+export default Carousel;
---
+++ src/pages/categories/Categories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Carousel from "../../components/carousel/Carousel";
+
+const Categories = () => {
+    return (
+        <>
+            <Carousel strCategory={"nome"} />
+        </>
+    );
+};
+
+export default Categories;
