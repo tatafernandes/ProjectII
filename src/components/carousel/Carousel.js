774b0867fe583b658e225d18e363bd9de0ff7a6c
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-17 19:18:10 -0400

    link css

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Carousel.css"
 
 const Carousel = ( {strCategory} ) => {
     return (
--- src/pages/categories/Categories.js
+++ src/pages/categories/Categories.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./Categories.css";
 import axios from "axios";
 import Carousel from "../../components/carousel/Carousel";
 
