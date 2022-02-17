a9a46014fb836e69dfd032e360b60ca598bc8fa6
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-17 19:13:47 -0400

    get categories

--- src/pages/categories/Categories.js
+++ src/pages/categories/Categories.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Carousel from "../../components/carousel/Carousel";
 
+class FoodsApi {
+    constructor () {
+        this.api = axios.create({ baseURL: "https://www.themealdb.com/api/json/v1/1" });
+    };
+
+    getCategories = async () => {
+        try {
+            const { data: { categories } } = await this.api.get("/categories.php");
+
+            return categories;
+        } catch (error) {
+            throw new Error(`Cannot Fetch Categories => ${error}`);
+        }
+    };
+};
+
+const foodsApi = new FoodsApi();
+
 const Categories = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => (async () => setCategories(await foodsApi.getCategories()))(), []);
+
+    console.log(categories);
+
     return (
         <>
             <Carousel strCategory={"nome"} />
