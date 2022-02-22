9fe242368469fc3a2a98ec62522c745cee8080c8
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-22 17:35:35 -0400

    test slider

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,9 +1,52 @@
 import React from "react";
 import "./Carousel.css"
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Carousel = ( {strCategory} ) => {
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
     return (
-        <h3>{strCategory}</h3>
+        <div>
+            <h2>{strCategory}</h2>
+            <Slider {...settings}>
+                {[1, 2, 3, 4, 5, 6, 7, 8].map(number => <p key={number}>{number}</p>)}
+            </Slider>
+        </div>
     );
 };
 
