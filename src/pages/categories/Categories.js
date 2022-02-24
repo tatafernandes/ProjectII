ff92b6f84b2c15b9a909a758e77bbd0293ea1512
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-24 19:25:43 -0400

    implement is Categories

--- src/pages/categories/Categories.js
+++ src/pages/categories/Categories.js
@@ -1,25 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Categories.css";
-import axios from "axios";
+import foodsApi from "../../utils/foodsApi";
 import Carousel from "../../components/carousel/Carousel";
-
-class FoodsApi {
-    constructor () {
-        this.api = axios.create({ baseURL: "https://www.themealdb.com/api/json/v1/1" });
-    };
-
-    getCategories = async () => {
-        try {
-            const { data: { categories } } = await this.api.get("/categories.php");
-
-            return categories;
-        } catch (error) {
-            throw new Error(`Cannot Fetch Categories => ${error}`);
-        }
-    };
-};
-
-const foodsApi = new FoodsApi();
 
 const Categories = () => {
     const [categories, setCategories] = useState([]);
--- src/utils/foodsApi.js
+++ src/utils/foodsApi.js
@@ -4,4 +4,16 @@ class FoodsApi {
     constructor () {
         this.api = axios.create({ baseURL: "https://www.themealdb.com/api/json/v1/1" });
     };
+    getCategories = async () => {
+        try {
+            const { data: { categories } } = await this.api.get("/categories.php");
+
+            return categories;
+        } catch (error) {
+            throw new Error(`Cannot Fetch Categories => ${error}`);
+        }
+    };
 };
+
+const foodsApi = new FoodsApi();
+export default foodsApi;
