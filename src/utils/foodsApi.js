d80534a91282c259a0072b14b3f3a410b47f0e61
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-24 19:55:04 -0400

    implement in FoodRecipe

--- src/utils/foodsApi.js
+++ src/utils/foodsApi.js
@@ -20,7 +20,47 @@ class FoodsApi {
             
             return meals;
         } catch (error) {
-            throw new Error(`Cannot Fetch ${category} => ${error}`);
+            throw new Error(`Cannot Fetch ${await category.strCategory} => ${error}`);
+        };
+    };
+
+    fetchRecipe = async id => {
+        try {
+            const { data: { meals } } = await this.api.get(`/lookup.php?i=${id}`);
+
+            return meals[0];
+        } catch (error) {
+            throw new Error(`Cannot Fetch recipe => ${error}`);
+        };
+    };
+
+    getIngredients = async meal => {
+        try {
+            const ingredients = [];
+            for (let i = 1; i <= 20; i += 1) {
+                const measure = await meal[`strMeasure${i}`];
+                const ingredient = await meal[`strIngredient${i}`].toLowerCase();
+                const notEmpty = (await measure !== "") && (await ingredient !== "");
+                const notNull = (await measure !== null) && (await ingredient !== null);
+
+                if (notEmpty && notNull) {
+                    ingredients.push({id: i, text: `${await measure} ${await ingredient}`})
+                };
+            };
+
+            return ingredients;
+        } catch (error) {
+            throw new Error(`Cannot Fetch ingredients => ${error}`);
+        };
+    };
+
+    getInstructions = async meal => {
+        try {
+            return await meal.strInstructions.split(/\r?\n/)
+                .filter(instruction => instruction !== "")
+                    .map((instruction, i) => {return { id: i + 1, paragraph: instruction }});
+        } catch (error) {
+            throw new Error(`Cannot Fetch instructions => ${error}`);
         };
     };
 };
