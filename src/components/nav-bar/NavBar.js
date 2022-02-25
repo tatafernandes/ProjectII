1df1805b8492f5212d0df0cf53423109e5964d8a
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-24 20:24:50 -0400

    add CategoriesButton to NavBar

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./NavBar.css";
 import { NavLink } from "react-router-dom";
+import CategoriesButton from "../categories-button/CategoriesButton";
 
 const NavBar = () => {
     return (
         <nav className="navbar-style">
             <NavLink to='/'><span> LOGO </span></NavLink>
-            <NavLink to='/food-carousel'>Categories</NavLink>
+            <CategoriesButton />
         </nav>
     );
 };
