950e9c57adda694231d94c027bf998c366b753bb
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-24 20:23:35 -0400

    create categories button with dropdown

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,15 +1,8 @@
-import React, { useState, useEffect } from "react";
-import foodsApi from "../../utils/foodsApi";
-import { NavLink } from "react-router-dom";
+import React from "react";
 import "./NavBar.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    const [categories, setCategories] = useState([]);
-
-    useEffect(() => (async () => setCategories(await foodsApi.getCategories()))(), []);
-
-    console.log(categories);
-
     return (
         <nav className="navbar-style">
             <NavLink to='/'><span> LOGO </span></NavLink>
