2606ea18829b66f83c1fda6602eb8203d2e0e7ab
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-25 17:25:47 -0400

    useState

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -4,11 +4,15 @@ import "./Search.css";
 const Search = () => {
     const [search, setSearch] = useState("");
 
-    console.log(search);
-
     return (
         <form className="control">
-            <input className="input is-warning is-rounded" type="text" placeholder="Search" value={search} onChange={e => setSearch(e.target.value)} />
+            <input
+                className="input is-warning is-rounded"
+                type="text"
+                placeholder="Type to search..."
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+            />
             <button className="button is-small is-rounded is-primary">Search</button>
         </form>
     );
