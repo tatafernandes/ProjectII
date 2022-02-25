da42c18b613e05b7bea1ad4734608cc65b350f88
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-25 16:49:42 -0400

    use Search component

---
+++ src/components/search/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Search.css";
+
+const Search = () => {
+    return (
+        <p className="control">
+            <input class="input is-warning" type="text" placeholder="Search" />
+        </p>
+    );
+};
+
+export default Search;
