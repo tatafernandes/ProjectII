2f6ded8c40b6610bdd63679cef79110611d357db
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-25 15:55:48 -0400

    add temporary searchBar

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -27,22 +27,7 @@ const NavBar = () => {
                     <div className="navbar-item">
                         <div className="field is-grouped">
                             <p className="control">
-                                <a className="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
-                                    <span className="icon">
-                                        <i className="fab fa-twitter"></i>
-                                    </span>
-                                    <span>
-                                        Tweet
-                                    </span>
-                                </a>
-                            </p>
-                            <p className="control">
-                                <a className="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.9.3/bulma-0.9.3.zip">
-                                    <span className="icon">
-                                        <i className="fas fa-download"></i>
-                                    </span>
-                                    <span>Download</span>
-                                </a>
+                                <input class="input is-warning" type="text" placeholder="Warning input" />
                             </p>
                         </div>
                     </div>
