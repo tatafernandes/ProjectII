c0f174a649852b21dbfdc3fe805d6b3424ac4108
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-25 11:24:06 -0400

    use bulma ins Instructions

--- src/components/instructions/Instructions.js
+++ src/components/instructions/Instructions.js
@@ -3,11 +3,11 @@ import "./Instructions.css";
 import ReactPlayer from "react-player/youtube";
 
 const Instructions = ({ instructions, strYoutube }) => (
-    <div>
-        <h2>Instructions</h2>
+    <section className="section content">
+        <h1 className="title">Instructions</h1>
         {instructions.map(instruction => <p key={instruction.id}>&emsp;{instruction.paragraph}</p>)}
         <ReactPlayer url={strYoutube} controls={true} />
-    </div>
+    </section>
 );
 
 
