d2ca6236b8667c8132f14d56e819f0840abc2612
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-24 20:12:58 -0400

    remove unnecessarry buttons

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import foodsApi from "../../utils/foodsApi";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
- export const NavBar = () => {
-    return <nav className="navbar-style">
-        
-        <NavLink to='/'><span> LOGO </span></NavLink>
-        <NavLink to='/foods-list/main-course'> Main Course </NavLink>
-        <NavLink to='/foods-list/Dessert'> Dessert </NavLink>
-        <NavLink to='/food-carousel'> Categories </NavLink>
-</nav> 
+const NavBar = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => (async () => setCategories(await foodsApi.getCategories()))(), []);
+
+    console.log(categories);
+
+    return (
+        <nav className="navbar-style">
+            <NavLink to='/'><span> LOGO </span></NavLink>
+            <NavLink to='/food-carousel'>Categories</NavLink>
+        </nav>
+    );
 };
 
 export default NavBar;
