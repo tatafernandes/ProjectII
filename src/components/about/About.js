64976921ab75836960f46461aa050c4564d1ed40
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-25 23:25:56 -0400

    remove css

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,16 +3,16 @@ import "./About.css";
 
 const About = () => {
     return (
-        <div className='about-us'>
         <div>
-            <h2>Thais Fernandes</h2>
-            <p>Brazilian, living in Portugal and trying to be a Web Development and a great mom! </p>
+            <div>
+                <h2>Thais Fernandes</h2>
+                <p>Brazilian, living in Portugal and trying to be a Web Development and a great mom! </p>
+            </div>
+            <div>
+                <h2>Vinicius Leite Batista</h2>
+                <p>Brazilian, from Boa Vista - Roraima, civil engineer student and Ironhacker Web Dev to be!</p>
+            </div>
         </div>
-        <div>
-            <h2>Vinicius Leite Batista</h2>
-            <p>Brazilian, from Boa Vista - Roraima, civil engineer student and Ironhacker Web Dev to be!</p>
-        </div>
-    </div>
     );
 };
 
