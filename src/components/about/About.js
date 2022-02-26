e9f77f5108180d3540c317de81171a13258ec433
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-25 23:57:32 -0400

    about us initial text

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,19 +1,30 @@
 import React from "react";
 import "./About.css";
+import logo from "../../utils/Thavi.png";
 
 const About = () => {
     return (
-        <div>
-            <div>
-                <h2>Thais Fernandes</h2>
-                <p>Brazilian, living in Portugal and trying to be a Web Development and a great mom! </p>
-            </div>
-            <div>
-                <h2>Vinicius Leite Batista</h2>
-                <p>Brazilian, from Boa Vista - Roraima, civil engineer student and Ironhacker Web Dev to be!</p>
+        <div id="about-us">
+            <figure className="content">
+                <img src={logo} alt="logo" />
+                <figcaption>Our logo</figcaption>
+            </figure>
+            <div className="content">
+                <h1 className="title">About us</h1>
+                <p>The ThaVi foods app was created for a</p>
             </div>
         </div>
     );
 };
 
 export default About;
+/*
+<div>
+    <h2>Thais Fernandes</h2>
+    <p>Brazilian, living in Portugal and trying to be a Web Development and a great mom! </p>
+</div>
+<div>
+    <h2>Vinicius Leite Batista</h2>
+    <p>Brazilian, from Boa Vista - Roraima, civil engineer student and Ironhacker Web Dev to be!</p>
+</div>
+*/
