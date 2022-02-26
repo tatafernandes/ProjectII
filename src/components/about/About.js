ea81b7003dbaef19e3ab38fea211d48bc2af2f62
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-26 01:31:29 -0400

    add logo to NavBar

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,6 @@ const About = () => {
         <div id="about-us">
             <figure className="content">
                 <img src={logo} alt="logo" />
-                <figcaption>Our logo</figcaption>
             </figure>
             <div className="content">
                 <h1 className="title">About us</h1>
@@ -46,13 +45,3 @@ const About = () => {
 };
 
 export default About;
-/*
-<div>
-    <h2>Thais Fernandes</h2>
-    <p>Brazilian, living in Portugal and trying to be a Web Development and a great mom! </p>
-</div>
-<div>
-    <h2>Vinicius Leite Batista</h2>
-    <p>Brazilian, from Boa Vista - Roraima, civil engineer student and Ironhacker Web Dev to be!</p>
-</div>
-*/
