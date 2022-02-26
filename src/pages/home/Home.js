1c768443cce4080ce343a31fa2437473d0f1a13c
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-02-25 22:49:40 -0400

    use Carousel in Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Home.css";
-//import FoodImages from "../../components/food-images/FoodImages";
 //import About from "../../components/about/About";
+import Carousel from "../../components/carousel/Carousel";
 
 const Home = () => {
     return (
-        <div className='home-js'>
-            <h1>Home</h1>
+        <div>
+            <Carousel category="RandomMeals" />
         </div>
     );
 };
